d31b0e10ab8812ecdc510dc17936ad1ecb8a99b3
Azie88 <obandoandrew8@gmail.com>
2024-01-27 14:21:22 +0300

    Simple openai prompt

--- summarize_text.py
+++ summarize_text.py
@@ -1,9 +1,13 @@
 from langchain.prompts import ChatPromptTemplate
 from langchain.chat_models import ChatOpenAI
+from decouple import config
 
 prompt = ChatPromptTemplate.from_template(
     "Please tell me which foods are famous in {place}"
 )
 
-model = ChatOpenAI()
+model = ChatOpenAI(openai_api_key=config("OPENAI_API_KEY"))
 chain = prompt | model
+
+result = chain.invoke({"place": "Kenya"})
+print(result.content)
